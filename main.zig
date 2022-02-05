8cf926df06f0afc6288022a893904f066c109774
Steven Schmid <stevschmid@gmail.com>
2022-02-05 15:42:28 +0100

    Threading

--- main.zig
+++ main.zig
@@ -48,52 +48,46 @@ pub fn Result(comptime A: type) type {
     };
 }
 
-// pub fn Worker(comptime DatabaseType: type) type {
-//     return struct {
-//         pub const WorkItem = struct {
-//             query: Sequence(DatabaseType.Alphabet),
-//         };
+pub fn Worker(comptime DatabaseType: type) type {
+    return struct {
+        pub const WorkItem = struct {
+            query: Sequence(DatabaseType.Alphabet),
+        };
 
-//         pub const WorkerContext = struct {
-//             allocator: std.mem.Allocator,
-//             queue: *std.atomic.Queue(WorkItem),
-//             database: *DatabaseType,
-//         };
+        pub const WorkerContext = struct {
+            allocator: std.mem.Allocator,
+            queue: *std.atomic.Queue(WorkItem),
+            database: *DatabaseType,
+        };
 
-//         fn entryPoint(context: *WorkerContext) !void {
-//             const allocator = context.allocator;
-//             const database = context.database;
+        fn entryPoint(context: *WorkerContext) !void {
+            const allocator = context.allocator;
+            const database = context.database;
 
-//             var search = try Search(DatabaseType).init(allocator, database, .{});
-//             defer search.deinit();
-//             _ = search;
+            var search = try Search(DatabaseType).init(allocator, database, .{});
+            defer search.deinit();
 
-//             while (context.queue.get()) |node| {
-//                 const query = node.data.query;
+            while (context.queue.get()) |node| {
+                const query = node.data.query;
 
-//                 var hits = SearchHitList.init(allocator);
-//                 defer hits.deinit();
-//                 try search.search(query, &hits);
+                var hits = SearchHitList(DatabaseType.Alphabet).init(allocator);
+                defer hits.deinit();
 
-//                 for (hits.list.items) |hit| {
-//                     std.debug.print("Hello {s}\n", .{hit.cigar.str()});
-//                 }
+                try search.search(query, &hits);
 
-//                 allocator.destroy(node);
-//             }
-//         }
-//     };
-// }
+                allocator.destroy(node);
+            }
+        }
+    };
+}
 
 pub fn main() !void {
     const databaseType = Database(alphabet.DNA, 8);
-    // const workerType = Worker(databaseType);
+    const workerType = Worker(databaseType);
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
-
-    var bench_start = std.time.milliTimestamp();
 
     var arg_it = std.process.args();
 
@@ -119,6 +113,8 @@ pub fn main() !void {
     defer allocator.free(filePathToOutput);
 
     // Read DB
+    var bench_start = std.time.milliTimestamp();
+
     var sequences = SequenceList(alphabet.DNA).init(allocator);
     defer sequences.deinit();
     try FastaReader(alphabet.DNA).readFile(filePathToDatabase, &sequences);
@@ -129,57 +125,73 @@ pub fn main() !void {
     try FastaReader(alphabet.DNA).readFile(filePathToQuery, &queries);
 
     print("Reading took {}ms\n", .{std.time.milliTimestamp() - bench_start});
+
     bench_start = std.time.milliTimestamp();
     var db = try databaseType.init(allocator, sequences.list.toOwnedSlice());
     defer db.deinit();
+
     print("Indexing took {}ms\n", .{std.time.milliTimestamp() - bench_start});
 
     // searching
 
-    // var queue = std.atomic.Queue(workerType.WorkItem).init();
-    // var context: workerType.WorkerContext = .{
-    //     .allocator = allocator,
-    //     .queue = &queue,
-    //     .database = &db,
-    // };
-
-    // for (queries.list.items) |query| {
-    //     const node = try context.allocator.create(std.atomic.Queue(workerType.WorkItem).Node);
-    //     node.* = .{
-    //         .data = workerType.WorkItem{
-    //             .query = query,
-    //         },
-    //     };
-    //     queue.put(node);
-    // }
-
-    // var thread = try std.Thread.spawn(.{}, workerType.entryPoint, .{&context});
-    // thread.join();
-
-    var search = try Search(databaseType).init(allocator, &db, .{});
-    defer search.deinit();
-
-    var hits = SearchHitList(alphabet.DNA).init(allocator);
-    defer hits.deinit();
-
-    var results = utils.ArrayListDeinitWrapper(Result(alphabet.DNA)).init(allocator);
-    defer results.deinit();
+    // Fill queue
+    var queue = std.atomic.Queue(workerType.WorkItem).init();
+    var context: workerType.WorkerContext = .{
+        .allocator = allocator,
+        .queue = &queue,
+        .database = &db,
+    };
 
     for (queries.list.items) |query| {
-        try search.search(query, &hits);
-
-        for (hits.list.items) |hit| {
-            std.debug.print("heh {s}\n", .{hit.cigar.str()});
-
-            try results.list.append(try Result(alphabet.DNA).init(allocator, query, hits));
-        }
+        const node = try context.allocator.create(std.atomic.Queue(workerType.WorkItem).Node);
+        node.* = .{
+            .data = workerType.WorkItem{
+                .query = query,
+            },
+        };
+        queue.put(node);
     }
 
-    const dir: std.fs.Dir = std.fs.cwd();
-    const file: std.fs.File = try dir.createFile(filePathToOutput, .{});
-    defer file.close();
+    // Search in threads
+    bench_start = std.time.milliTimestamp();
 
-    for (results.list.items) |result| {
-        try AlnoutWriter(alphabet.DNA).write(file.writer(), result.query, result.hits);
+    const worker_count = std.math.max(1, std.Thread.getCpuCount() catch 1);
+    const threads = try allocator.alloc(std.Thread, worker_count);
+    defer allocator.free(threads);
+
+    for (threads) |*thread| {
+        thread.* = try std.Thread.spawn(.{}, workerType.entryPoint, .{&context});
     }
+
+    for (threads) |thread| {
+        thread.join();
+    }
+
+    print("Searching took {}ms\n", .{std.time.milliTimestamp() - bench_start});
+
+    //     var search = try Search(databaseType).init(allocator, &db, .{});
+    //     defer search.deinit();
+
+    //     var results = utils.ArrayListDeinitWrapper(Result(alphabet.DNA)).init(allocator);
+    //     defer results.deinit();
+
+    //     bench_start = std.time.milliTimestamp();
+    //     for (queries.list.items) |query| {
+    //         var hits = SearchHitList(alphabet.DNA).init(allocator);
+    //         defer hits.deinit();
+
+    //         try search.search(query, &hits);
+
+    //         try results.list.append(try Result(alphabet.DNA).init(allocator, query, hits));
+    //     }
+
+    //     const dir: std.fs.Dir = std.fs.cwd();
+    //     const file: std.fs.File = try dir.createFile(filePathToOutput, .{});
+    //     defer file.close();
+
+    //     bench_start = std.time.milliTimestamp();
+    //     for (results.list.items) |result| {
+    //         try AlnoutWriter(alphabet.DNA).write(file.writer(), result.query, result.hits);
+    //     }
+    //     print("Writing took {}ms\n", .{std.time.milliTimestamp() - bench_start});
 }
